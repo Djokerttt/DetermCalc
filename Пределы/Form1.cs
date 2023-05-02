d26444c9dad5be9583475f45be3f1502b30f258b
Herecore <herecore@protonmail.com>
2023-05-02 23:46:43 +0300

    refactoring

--- Пределы/Form1.cs
+++ Пределы/Form1.cs
@@ -36,18 +36,10 @@ namespace Пределы
             }
         }
 
-        private void equals_button_Click(object sender, EventArgs e)
-        {
-            //// Нажатие по кнопке равно
-            // Получаем значения числителя и знаменателя и записываем в удобоваримый для бекенда вид
-            string numerator = numeratorInput.Text;
-            string denominator = denominatorInput.Text;
-            string args = $"\"{numerator}\" \"{denominator}\"";
-			
-			///////////////////////////////////////
-			////////// Работа с бекендом //////////
-			////////////////////////////////////////
-            // создаем процесс
+		// Работа с бекендом
+		static string runBackend(string args)
+		{
+            // Создаем процесс
             Process process = new Process();
 
             // Свойства процесса
@@ -56,78 +48,86 @@ namespace Пределы
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
 
-            // запускаем процесс
+            // Запускаем процесс
             process.Start();
 
             // получаем выходные данные
             string output = process.StandardOutput.ReadToEnd();
 
-            // ожидаем завершения процесса
+            // Ожидаем завершения процесса
             process.WaitForExit();
-			////////////////////////////////////////
+			
+			// Возвращаем то, что выдал бекенд
+			return output;
+		}
+		
+		static string fitSeparator(string numerator, string denominator, string separator)
+		{
+			// Ищем что длиннее, числитель или знаменатель, и подгоняем длину разделителя под количество символов
+            int maxLength = Math.Max(numerator.Length, denominator.Length);
+            for (int i = 0; i < maxLength; i++)
+            {
+                separator += "-";
+            }
+			return separator;
+		}
 
-			///////////////////////////////////////
-            ////// вывод ответа в answerBox ///////
-			///////////////////////////////////////
-			// Работаем с двумя последними строками вывода
-            string[] lines = output.Split('\n');
-            string answerNumerator = lines[lines.Length - 3].TrimEnd();
-            string answerDenominator = lines[lines.Length - 2].TrimEnd();
-
-            /// Упростим числитель и знаменатель
+		static void printToAnswerBox(string numerator, string denominator, string answerNumerator, string answerDenominator)
+		{
+            /// Упростим числитель и знаменатель, разделив оба на наибольший общий делитель (gcd)
             int tmp1 = int.Parse(answerNumerator);
             int tmp2 = int.Parse(answerDenominator);
-            // наибольший общий делитель
             int gcd = Gcd(tmp1, tmp2);
             tmp1 /= gcd;
             tmp2 /= gcd;
             answerNumerator = tmp1.ToString();
             answerDenominator = tmp2.ToString();
 
-
-            /// создать separator (знак деления)
+            /// Создать separator (знак деления)
             string separator = "";
 
-			// Ищем что длиннее, числитель или знаменатель, и подгоняем длину полосы под количество символов
-            int maxLengthProblem = Math.Max(numerator.Length, denominator.Length);
-            for (int i = 0; i < maxLengthProblem; i++)
-            {
-                separator += "-";
-            }
+			// Ищем что длиннее, числитель или знаменатель, и подгоняем длину разделителя под количество символов
+			separator = fitSeparator(numerator, denominator, separator);
             separator += " = ";
 
-            // создаем часть уровнения после равно
-            int maxLengthAnswer = Math.Max(answerNumerator.Length, answerDenominator.Length);
-            for (int i = 0; i < maxLengthAnswer; i++)
-            {
-                separator += "-";
-            }
-
-            // выровняем числитель и знаменатель по длине пробелами
-			string tmpNumerator = numerator;
-			string tmpDenominator = denominator;
+            // Выровняем числитель и знаменатель по длине пробелами
             if (numerator.Length < maxLengthProblem)
             {
-            	tmpNumerator = tmpNumerator.PadRight(maxLengthProblem);
+            	numerator = numerator.PadRight(maxLengthProblem);
             }
             else if (denominator.Length < maxLengthProblem)
             {
-                tmpDenominator = tmpDenominator.PadRight(maxLengthProblem);
+                denominator = denominator.PadRight(maxLengthProblem);
             }
+
+            // создаем часть уровнения после равно
+			separator += fitSeparator(answerNumerator, answerDenominator, separator);
 			
-			// компенсируем для " = " в separator
-            tmpNumerator += "   " + answerNumerator;
-            tmpDenominator += "   " + answerDenominator;
+			// компенсируем для " = " в separator и добавляем ответ
+            numerator += "   " + answernumerator;
+            denominator += "   " + answerdenominator;
 
 			// Выводим ответ
-            string result_output = $"{tmpNumerator}\r\n{separator}\r\n{tmpDenominator}";
+            string result_output = $"{numerator}\r\n{separator}\r\n{denominator}";
             answerBox.Text = result_output;
-			///////////////////////////////////////
-			
-			///////////////////////////////////////
-			///////// Вывод шагов решения /////////
-			///////////////////////////////////////
-			//// Выводим первый шаг, деление числителя и знаменателя на наибольшую степень
+		}
+
+		static void divideByX(string largestPow)
+		{
+			if (largestPow == "1")
+			{
+				tmpoutput = $"     x\r\n";
+				solvingStepsBox.AppendText(tmpoutput);
+			}
+			else
+			{
+				tmpoutput = $"     x^{largestPow}\r\n";
+				solvingStepsBox.AppendText(tmpoutput);
+			}
+		}
+
+		static void printToSolvingStepsFirst(string numerator, string denominator, string[] lines)
+		{
 			// Взять наибольшую степень из output
             string largestPow = lines[0].TrimEnd();
 
@@ -146,34 +146,17 @@ namespace Пределы
 
 			/// Пункт 2
 			// Создаем дробь в ширину числителя
-			separator = "";
-            for (int i = 0; i < numerator.Length; i++)
-            {
-                separator += "-";
-            }
+			string separator = fitSeparator(numerator, 0);
 			tmpoutput = $"     {separator}\r\n";
 			solvingStepsBox.AppendText(tmpoutput);
 			
 			/// Пункт 3
-			if (largestPow == "1")
-			{
-				tmpoutput = $"     x\r\n";
-				solvingStepsBox.AppendText(tmpoutput);
-			}
-			else
-			{
-				tmpoutput = $"     x^{largestPow}\r\n";
-				solvingStepsBox.AppendText(tmpoutput);
-			}
+			divideByX(largestPow);
 
 			/// Пункт 4
-			// Ищем что длиннее, числитель или знаменатель, и подгоняем длину полосы под количество символов
-			string wideSeparator = "";
-            for (int i = 0; i < maxLengthProblem; i++)
-            {
-                wideSeparator += "-";
-            }
-			tmpoutput = $"lim -{wideSeparator}- =\r\n";
+			// Создаем дробь посередине
+			separator = fitSeparator(numerator, denominator);
+			tmpoutput = $"lim -{separator}- =\r\n";
 			solvingStepsBox.AppendText(tmpoutput);
 
 			/// Пункт 5
@@ -182,74 +165,86 @@ namespace Пределы
 			
 			/// Пункт 6
 			// Создаем дробь в ширину знаменателя
-			separator = "";
-            for (int i = 0; i < denominator.Length; i++)
-            {
-                separator += "-";
-            }
+			separator = fitSeparator(0, denominator);
 			tmpoutput = $"     {separator}\r\n";
 			solvingStepsBox.AppendText(tmpoutput);
 
 			/// Пункт 7 (то же самое что пункт 3)
-			if (largestPow == "1")
-			{
-				tmpoutput = $"     x\r\n";
-				solvingStepsBox.AppendText(tmpoutput);
-			}
-			else
-			{
-				tmpoutput = $"     x^{largestPow}\r\n";
-				solvingStepsBox.AppendText(tmpoutput);
-			}
+			divideByX(largestPow);
 			solvingStepsBox.AppendText("\r\n");
+		}
 
-			//// Выводим второй шаг, промежуточный результат после деления, что стало 0
+		static void printToSolvingStepsSecond(string[] lines)
+		{
 			/// Выводим в таком порядке:
 			/* 5 пробелов + промежуточный числитель
 			 * "lim" + пробел + дробь в ширину самого широкого числителя/знаменателя + 2x"-" + " ="
 			 * "x→∞" + 2 пробела + промежуточный знаменатель */
 
-            string intermNumerator = lines[2].TrimEnd();
-            string intermDenominator = lines[3].TrimEnd();
+            string numerator = lines[2].TrimEnd();
+            string denominator = lines[3].TrimEnd();
 			
 			/// Пункт 1
-			tmpoutput = $"     {intermNumerator}\r\n";
+			tmpoutput = $"     {numerator}\r\n";
 			solvingStepsBox.AppendText(tmpoutput);
 
 			/// Пункт 2
 			// Ищем что длиннее, числитель или знаменатель, и подгоняем длину полосы под количество символов
-			wideSeparator = "";
-            int maxLengthInterm = Math.Max(intermNumerator.Length, intermDenominator.Length);
-            for (int i = 0; i < maxLengthInterm; i++)
-            {
-                wideSeparator += "-";
-            }
-			tmpoutput = $"lim -{wideSeparator}- =\r\n";
+			separator = fitSeparator(numerator, denominator);
+			tmpoutput = $"lim -{separator}- =\r\n";
 			solvingStepsBox.AppendText(tmpoutput);
 
 			/// Пункт 3
-			tmpoutput = $"x→∞  {intermDenominator}\r\n";
+			tmpoutput = $"x→∞  {denominator}\r\n";
 			solvingStepsBox.AppendText(tmpoutput);
 
 			solvingStepsBox.AppendText("\r\n");
+		}
 
-
-			//// Выводим третий шаг, просто ответ
-			tmpoutput = $"{answerNumerator}\r\n";
+		static void printToSolvingStepsThird(string numerator, string denominator)
+		{
+			tmpoutput = $"{numerator}\r\n";
 			solvingStepsBox.AppendText(tmpoutput);
 
 			// Дробь в ширину самого длинного элемента
-			separator = "";
-            for (int i = 0; i < maxLengthAnswer; i++)
-            {
-                separator += "-";
-            }
+			separator = fitSeparator(numerator, denominator);
 			tmpoutput = $"{separator}\r\n";
 			solvingStepsBox.AppendText(tmpoutput);
 
-			tmpoutput = $"{answerDenominator}\r\n";
+			tmpoutput = $"{denominator}\r\n";
 			solvingStepsBox.AppendText(tmpoutput);
-			///////////////////////////////////////
+		}
+
+        private void equals_button_Click(object sender, EventArgs e)
+        {
+            //// Нажатие по кнопке равно
+            // Получаем значения числителя и знаменателя и записываем в удобоваримый для бекенда вид
+            string numerator = numeratorInput.Text;
+            string denominator = denominatorInput.Text;
+            string args = $"\"{numerator}\" \"{denominator}\"";
+			
+			// Запускаем бекенд, получаем вывод в output
+			string output = runBackend(args)
+
+			// Разделяем вывод бекенда на строки 
+            string[] lines = output.Split('\n');
+
+			//// Выводим ответ в AnswerBox
+			// Работаем с двумя последними строками вывода
+			// answerX - то что вывел бекенд, когда просто numerator и denominator - то что ввёл юзер
+            string answerNumerator = lines[lines.Length - 3].TrimEnd();
+            string answerDenominator = lines[lines.Length - 2].TrimEnd();
+			printToAnswerBox(numerator, denominator, lines);
+
+			//// Выводим шаги решения в solvingStepsBox
+			// Выводим первый шаг, деление числителя и знаменателя на наибольшую степень
+			printToSolvingStepsFirst(numerator, denominator, lines);
+
+			// Выводим второй шаг, промежуточный результат после деления, что стало 0
+			printToSolvingStepsSecond(lines);
+
+			// Выводим третий шаг, просто ответ
+			printToSolvingStepsThird(answerNumerator, answerDenominator);
         }
     }
 }
